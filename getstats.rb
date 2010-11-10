a9b80043bcd0bf6484df82d63760aa00698c7b9f
Davy Landman <davy.landman@gmail.com>
2010-11-10 09:13:37 +0100

    Fixed identing to use ruby standards

--- getstats.rb
+++ getstats.rb
@@ -6,16 +6,16 @@ extensions = %w[3fr ari arw srf sr2 bay crw cr2 cap iiq eip dcs
  dcr drf k25 kdc dng erf fff mef mos mrw nef nrw orf ptx pef pxn R3D 
  raf raw rw2 raw rwl rwz x3f jpg jpeg tiff]
 if ARGV.length != 2
-	puts "Usage: ./getstats.rb <root directory> <sqlite db>"
-	return
+  puts "Usage: ./getstats.rb <root directory> <sqlite db>"
+  return
 end
 
 db = SQLite3::Database.new(ARGV[1])
 begin
-	#if the DB didn't exist, lets add the table.
-	db.execute("CREATE TABLE photo_stats 
-		(Filename TEXT UNIQUE, Lens TEXT, FocalLength TEXT, 
-		Aperture TEXT, ShutterSpeed TEXT, HyperFocalDistance TEXT)")
+  #if the DB didn't exist, lets add the table.
+  db.execute("CREATE TABLE photo_stats 
+    (Filename TEXT UNIQUE, Lens TEXT, FocalLength TEXT, 
+    Aperture TEXT, ShutterSpeed TEXT, HyperFocalDistance TEXT)")
 rescue
 end
 rootdir = ARGV[0]
@@ -24,21 +24,21 @@ images = Dir[rootdir + "**/*.{#{extensions.join(',')}}"]
 puts "Extracting information out of #{images.length} images"
 add_new_image = db.prepare('INSERT INTO photo_stats VALUES (:filename, :lens, :focal, :aperture, :shutterspeed, :hyperfocaldistance)')
 images.each_index do |index|
-	current_image = images[index]
-	ex = MiniExiftool.new(current_image)
-	if not ex['Lens'].nil? and not ex['Lens'].empty?
-		begin
-			add_new_image.execute(:filename => current_image,
-								  :lens => ex['Lens'],
-								  :focal => ex['FocalLength'],
-								  :aperture => ex['Aperture'],
-								  :shutterspeed => ex['ShutterSpeed'],
-								  :hyperfocaldistance => ex['HyperFocalDistance'])
-		rescue
-		end
-	end
-	printf("%.2f%% ", (index.to_f / images.length) * 100)
-	puts "" if (index % 10) == 0
+  current_image = images[index]
+  ex = MiniExiftool.new(current_image)
+  if not ex['Lens'].nil? and not ex['Lens'].empty?
+    begin
+      add_new_image.execute(:filename => current_image,
+	:lens => ex['Lens'],
+	:focal => ex['FocalLength'],
+	:aperture => ex['Aperture'],
+	:shutterspeed => ex['ShutterSpeed'],
+	:hyperfocaldistance => ex['HyperFocalDistance'])
+    rescue
+    end
+  end
+  printf("%.2f%% ", (index.to_f / images.length) * 100)
+  puts "" if (index % 10) == 0
 end
 puts "" if (images.length % 10) != 0 
 add_new_image.close()
